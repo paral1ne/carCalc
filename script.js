7865462b2875fc3885a6eb8e0320000f46d1d9b7
Androff <135033278+paral1ne@users.noreply.github.com>
2023-12-22 20:13:00 +0100

    1. 14 F

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function fuelCount() {
   var distance = parseFloat(document.getElementById("distance").value);
   var consumption = parseFloat(document.getElementById("consumption").value);
   var fuelPrice = parseFloat(document.getElementById("fuelPrice").value);
-
-  var summ = Math.round((distance / 100) * consumption * fuelPrice);
-  document.getElementById("result").innerHTML =
-    "Стоимость топлива: " + summ + " денег";
+  if (!isNaN(distance) && !isNaN(consumption) && !isNaN(fuelPrice)) {
+    var summ = Math.round((distance / 100) * consumption * fuelPrice);
+    document.getElementById("result").innerHTML =
+      "Стоимость топлива: " + summ + " денег";
+  } else {
+    document.getElementById("result").innerHTML = "Я понимаю только числа";
+  }
 }
